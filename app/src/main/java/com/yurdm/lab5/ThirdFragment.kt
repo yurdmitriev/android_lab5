3eb0e7f8cd69e8864a113356534e52cbe77afb4c
yurdmitriev <yurdmitriev02@gmail.com>
2022-05-21 03:13:44 +0300

    Remove unused code

--- app/src/main/java/com/yurdm/lab5/ThirdFragment.kt
+++ app/src/main/java/com/yurdm/lab5/ThirdFragment.kt
@@ -3,16 +3,4 @@ package com.yurdm.lab5
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 
-class ThirdFragment : Fragment(R.layout.fragment_third) {
-    companion object {
-        fun newInstance(position: Int): ThirdFragment {
-            val args = Bundle()
-            args.putInt("position", position)
-
-            val fragment = ThirdFragment()
-            fragment.arguments = args
-
-            return fragment
-        }
-    }
-}
+class ThirdFragment : Fragment(R.layout.fragment_third)
